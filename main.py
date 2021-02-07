2c37d6e44887b58176e9e9fce2ffc04f2090a764
duyminh1998 <minhhua12345@gmail.com>
2021-02-07 05:10:49 -0600

    started scraping wsb:

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World");
+# print("Hello World");
+
+import praw
+
+reddit = praw.Reddit(
+    client_id="vvduT3kVaQ7tqQ",
+    client_secret="oWHlCFPkRROIOfAlF12duFXrKGvEaw",
+    user_agent="pc:https://github.com/duyminh1998/wsb-gpt-2:v1 (by /u/duyminh1998)",
+)
+
+# print(reddit.read_only)  # Output: True
+
+wsb = reddit.subreddit("wallstreetbets")
+
+for submission in wsb.hot(limit=10):
+    print(submission.title)
