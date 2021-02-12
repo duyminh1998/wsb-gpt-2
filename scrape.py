2f5dce6abb62a148b8b644b461caae8ab26b2c1f
duyminh1998 <minhhua12345@gmail.com>
2021-02-12 14:57:58 -0600

    finalized scraping method and started gpt-2 integration

--- scrape.py
+++ scrape.py
@@ -34,26 +34,26 @@ api = PushshiftAPI()
 start_epoch= int(dt.datetime(2013, 1, 1).timestamp())
 end_epoch = int(dt.datetime(2013, 2, 1).timestamp())
 
-results = api.search_submissions(q='DD', after=start_epoch,
-	                            subreddit='wallstreetbets',
-	                            filter=['author', 'title', 'selftext', 'link_flair_text', 'id', 'created'],
-	                            limit=10)
-
-df2 = pd.DataFrame([thing.d_ for thing in results])
-print("Initialized data frame.")
-
-for j in range(9):
-	year = str(2013 + j)
-	print("Year: " + year)
-	for i in range(1, 11):
-		print("Month: " + str(i))
-		results = api.search_submissions(q='DD', after=start_epoch, before=end_epoch,
+results = api.search_submissions(q='flair%3ADD', after=start_epoch,
 	                            subreddit='wallstreetbets',
 	                            filter=['author', 'title', 'selftext', 'link_flair_text', 'id', 'created'],
 	                            limit=1000)
-		start_epoch = end_epoch
-		end_epoch = int(dt.datetime(2013 + j, i + 2, 1).timestamp())
-		df = pd.DataFrame([thing.d_ for thing in results])
-		df2.append(df, sort=False)
+
+df2 = pd.DataFrame([thing.d_ for thing in results])
+# print("Initialized data frame.")
+
+# for j in range(9):
+# 	year = str(2013 + j)
+# 	print("Year: " + year)
+# 	for i in range(1, 11):
+# 		print("Month: " + str(i))
+# 		results = api.search_submissions(q='DD', after=start_epoch, before=end_epoch,
+# 	                            subreddit='wallstreetbets',
+# 	                            filter=['author', 'title', 'selftext', 'link_flair_text', 'id', 'created'],
+# 	                            limit=1000)
+# 		start_epoch = end_epoch
+# 		end_epoch = int(dt.datetime(2013 + j, i + 2, 1).timestamp())
+# 		df = pd.DataFrame([thing.d_ for thing in results])
+# 		df2.append(df, sort=False)
 
 df2.to_csv('results.csv', index=False, encoding='utf-8')
